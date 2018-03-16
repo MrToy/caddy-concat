0ad8bcd7d15c572614a02c090fc4654065ca4868
Toy <1659808224@qq.com>
2018-03-16 11:13:34 +0800

    fix bug

--- plugin.go
+++ plugin.go
@@ -2,30 +2,34 @@ package concat
 
 import (
 	//"fmt"
-	"github.com/mholt/caddy"
-	"github.com/mholt/caddy/caddyhttp/httpserver"
+	"fmt"
 	"io"
 	"net/http"
 	"net/url"
 	"path"
 	"strings"
+
+	"github.com/mholt/caddy"
+	"github.com/mholt/caddy/caddyhttp/httpserver"
 )
 
 type Concat struct {
 	Next httpserver.Handler
 }
 
-func (this Concat) ServeHTTP(w http.ResponseWriter, r *http.Request) (int, error) {
-	if strings.Contains(r.URL.String(), "??") {
-		files := strings.Split(strings.Split(r.URL.RawQuery, "?")[1], ",")
+func (ctx Concat) ServeHTTP(w http.ResponseWriter, r *http.Request) (int, error) {
+	if strings.Contains(r.RequestURI, "??") {
+		chunk := strings.Split(r.RequestURI, "??")
+		files := strings.Split(chunk[1], ",")
 		for _, file := range files {
 			u := url.URL{
-				Path:   path.Join(r.URL.Path, file),
+				Path:   path.Join(chunk[0], file),
 				Host:   r.Host,
 				Scheme: "http",
 			}
 			resp, err := http.Get(u.String())
 			if err != nil {
+				fmt.Println(err)
 				return 500, err
 			}
 			io.Copy(w, resp.Body)
@@ -33,7 +37,7 @@ func (this Concat) ServeHTTP(w http.ResponseWriter, r *http.Request) (int, error
 		}
 		return 200, nil
 	}
-	return this.Next.ServeHTTP(w, r)
+	return ctx.Next.ServeHTTP(w, r)
 }
 
 func init() {
