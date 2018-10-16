8c4884a5131b405d9a4fda3ca31305b419e73123
Toy <1659808224@qq.com>
2018-10-16 14:28:05 +0800

    fix bug

--- plugin.go
+++ plugin.go
@@ -21,17 +21,20 @@ func (ctx Concat) ServeHTTP(w http.ResponseWriter, r *http.Request) (int, error)
 	if strings.Contains(r.RequestURI, "??") {
 		chunk := strings.Split(r.RequestURI, "??")
 		files := strings.Split(chunk[1], ",")
+		pre, _ := url.Parse(chunk[0])
 		for _, file := range files {
 			u := url.URL{
-				Path:   path.Join(chunk[0], strings.Split(file, "?")[0]),
+				Path:   path.Join(pre.Path, strings.Split(file, "?")[0]),
 				Host:   r.Host,
 				Scheme: "http",
 			}
 			resp, err := http.Get(u.String())
+			contentType := resp.Header.Get("Content-Type")
 			if err != nil {
 				fmt.Println(err)
 				return 500, err
 			}
+			w.Header().Set("Content-Type", contentType)
 			io.Copy(w, resp.Body)
 			w.Write([]byte("\n"))
 		}
